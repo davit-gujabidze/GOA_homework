71c3639fa15eae955f39cf653637d61cb32b63c2
davit-gujabidze <gujijaba@gmail.com>
2025-09-12 17:22:48 +0400

    level1

---
+++ level 9/homework/homework.py
@@ -0,0 +1,17 @@
+# 0) For loop explanation:
+# A for loop repeats a block of code for each item in a sequence.
+
+for i in range(5):
+    print(i)
+
+for _ in range(5):
+    print("study")
+
+for i in range(10):
+    print(i)
+
+for _ in range(7):
+    print("hello")
+
+for _ in range(3):
+    print("loop")
