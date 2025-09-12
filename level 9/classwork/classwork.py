3a5c4d02be57850da70e25e30063d08ed80e92eb
davit-gujabidze <gujijaba@gmail.com>
2025-09-12 17:34:27 +0400

    level1

---
+++ level 9/classwork/classwork.py
@@ -0,0 +1,31 @@
+# Control Flow (Simple)
+
+# if
+# if x>0: print("yes")
+
+# if-else
+# if x>0: print("yes")
+# else: print("no")
+
+# if-elif-else
+# if x>0: print("pos")
+# elif x<0: print("neg")
+# else: print("zero")
+
+# for
+# for i in range(5): print(i)
+
+# while
+# while x>0: x-=1
+
+# break
+# for i in range(5):
+#     if i==3: break
+
+# continue
+# for i in range(5):
+#     if i==2: continue
+#     print(i)
+
+# pass
+# if x>0: pass
