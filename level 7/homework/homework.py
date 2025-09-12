847b0777a3fe6a9da55d2f75b1c318054e6781e2
davit-gujabidze <gujijaba@gmail.com>
2025-09-12 17:12:30 +0400

    level1

---
+++ level 7/homework/homework.py
@@ -0,0 +1,15 @@
+x = 5
+y = 10
+print(x < y)
+a = 7
+b = 3
+print(a > b)
+age = 17
+has_id = False
+is_student = True
+print(age >= 18 and has_id)
+likes_chocolate = True
+eats_healthy = False
+print(likes_chocolate or eats_healthy)
+is_active = False
+print(not is_active)
